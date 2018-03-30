0c860e3ae6dc254b7f37b0003361468cc68bf869
David Hovey <david@hoveytech.com>
2018-03-30 08:26:51 -0700

    Added 2018 full test, fixed 2017 message

--- AutoCad2018Test/Class1.cs
+++ AutoCad2018Test/Class1.cs
@@ -1,17 +1,17 @@
-﻿using AutoCad2017LightTest;
+﻿using AutoCad2018Test;
 using Autodesk.AutoCAD.Runtime;
 using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
 
 [assembly: ExtensionApplication(typeof(Class1))]
 
-namespace AutoCad2017LightTest
+namespace AutoCad2018Test
 {
     public class Class1 : IExtensionApplication
     {
         public void Initialize()
         {
             var editor = Application.DocumentManager.MdiActiveDocument.Editor;
-            editor.WriteMessage("Hello from .NET Extension, 2017");
+            editor.WriteMessage("Hello from .NET Extension, 2018");
         }
 
         public void Terminate()
