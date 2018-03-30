b13cf8a70ca78ce523fbbd1b33a9e2c9fddaebd3
David Hovey <david@hoveytech.com>
2018-03-30 08:02:23 -0700

    Initial commit, Including 2017 as baseline

---
+++ AutoCad2017LightTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using AutoCad2017LightTest;
+using Autodesk.AutoCAD.Runtime;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+[assembly: ExtensionApplication(typeof(Class1))]
+
+namespace AutoCad2017LightTest
+{
+    public class Class1 : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Hello from .NET Extension, 2018");
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
